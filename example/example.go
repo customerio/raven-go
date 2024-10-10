626c1cf82f59582d277185e8df525b43f0f625f4
Richard Dawe <rich.dawe@customer.io>
2024-10-10 11:03:55 +0100

    Add go.mod, etc. Fix error in example.

--- example/example.go
+++ example/example.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/getsentry/raven-go"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/richdawe-cio/raven-go"
 )
 
 func trace() *raven.Stacktrace {
@@ -31,9 +32,9 @@ func main() {
 
 // CheckError sends error report to sentry and records event id and error name to the logs
 func CheckError(err error, r *http.Request) {
-	client, err := raven.NewWithTags(os.Args[1], map[string]string{"foo": "bar"})
-	if err != nil {
-		log.Fatal(err)
+	client, clientErr := raven.NewWithTags(os.Args[1], map[string]string{"foo": "bar"})
+	if clientErr != nil {
+		log.Fatal(clientErr)
 	}
 	packet := raven.NewPacket(err.Error(), raven.NewException(err, trace()), raven.NewHttp(r))
 	eventID, _ := client.Capture(packet, nil)
